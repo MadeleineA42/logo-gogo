a4178c6291517468292b5f10a4356e18058812ae
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-02 16:59:17 -0400

    made some adjustments to render the logo correctly

--- index.js
+++ index.js
@@ -49,7 +49,13 @@ async function main() {
         }
 
         //Generate SVG code and save it to logo.svg
-        const svgCode = selectedShape.render();
+        
+        const svgCode = `<svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+        ${selectedShape.render()}
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${userInput.textColor}">${userInput.text}</text>
+</svg>`;
         fs.writeFileSync('logo.svg', svgCode);
 
         console.log('Generated logo.svg file');
